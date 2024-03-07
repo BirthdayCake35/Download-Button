86218be828082b246dec7bdb455814794ea67874
Admin <admin@Utkan-Mac-mini.local>
2024-03-07 15:55:20 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Cod3 Challenge
+
+This challenge is to make sample projects that people can use. Today we made an application that will download at 5 second intervals when you press the "Download" button.
+
+Developed by <b>BirthdayCake</b>
+
+[SUPPORT ME](mailto:utku0iletisim@gmail.com)
---
+++ download.js
@@ -0,0 +1,27 @@
+const downloadBtn = document.getElementById('downloadBtn');
+const fileLink = './download-example.txt'; 
+
+downloadBtn.addEventListener('click', (event) => {
+    event.preventDefault(); 
+    downloadBtn.disabled = true; 
+
+    let timer = 5; 
+
+
+    const initCounter = setInterval(() => {
+        if (timer > 0) {
+            downloadBtn.innerHTML = `Your Download will begin in <b>${timer}</b> seconds`;
+            timer--;
+        } else {
+            clearInterval(initCounter); 
+            downloadBtn.innerHTML = 'Your file is downloading...'; 
+
+            
+            const link = document.createElement('a');
+            link.href = fileLink;
+            link.download = 'example'; 
+            document.body.appendChild(link);
+            link.click();
+        }
+    }, 1000); 
+});
